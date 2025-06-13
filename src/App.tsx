339e531168c10953d486bad05b25091dc4514b71
Taye Birhanu <tayyebirhan@gmail.com>
2025-06-13 18:12:20 +0300

    delete unesesary files add esbuild-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react"
+import * as esbuild from 'esbuild-wasm'
 function App() {
-  const [count, setCount] = useState(0)
+
+  const startSevice = async () => {
+    const service = await esbuild.initialize({
+      worker: true,
+      wasmURL: './esbuild.wasm'
+    })
+    console.log(service)
+  }
+
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+  useEffect(() => {
+
+  }, [])
+
+  const onClick = () => {
+    // setCode(input)
+    console.log(input)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea value={input} onChange={event => setInput(event.target.value)} name="" id="" rows={10} cols={50}></textarea>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <button onClick={onClick} type="submit">Submit</button>
+      <div>
+        <pre>{'./esbuild.wasm'}</pre>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
